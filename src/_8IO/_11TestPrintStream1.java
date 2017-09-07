8a217474ec316cf6895f25b9a60cfb7aed824970
admin <jiangdou3976@163.com>
2017-09-07 18:25:18 +0800

    0907_ObjectIO

--- src/_8IO/_11TestPrintStream1.java
+++ src/_8IO/_11TestPrintStream1.java
@@ -10,15 +10,15 @@ public class _11TestPrintStream1 {
             FileOutputStream fos = new FileOutputStream("d:/javatest/printStream.txt");
             PrintStream ps = new PrintStream(fos);
             if (ps != null) {
-                System.setOut(ps);
+                System.setOut(ps);//将原来打印的地方改成ps所在的位置
             }
-            int ln=0;
-            for (int i=0;i<1000;i++) {
-                System.out.print(i+"\t");
+            int ln = 0;
+            for (int i = 0; i < 1000; i++) {
+                System.out.print(i + "\t");
                 ln++;
-                if ( ln> 100) {
+                if (ln > 100) {
                     System.out.println();
-                    ln=0;
+                    ln = 0;
                 }
             }
         } catch (FileNotFoundException e) {
---
+++ src/_8IO/_12TestObjectIO.java
@@ -0,0 +1,44 @@
+package _8IO;
+
+import java.io.*;
+
+/**
+ * Created by admin on 2017/9/7.
+ */
+public class _12TestObjectIO {
+    public static void main(String[] args) {
+        Test t = new Test();
+        try {
+            FileOutputStream fos = new FileOutputStream("d:/javatestfiles/ObjectIO.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(t);
+            oos.flush();
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("d:/javatestfiles/ObjectIO.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Test ttt =(Test)ois.readObject();
+            System.out.println("ttt.age="+ttt.age);
+            System.out.println("ttt.name="+ttt.name);
+            System.out.println("ttt.weight="+ttt.weight);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+/**
+ * Test需要实现序列化接口，否则会报  java.io.NotSerializableException  异常
+ */
+class Test {
+    String name = "zhangsan";
+    int age=18;
+    double weight = 20.5;
+}
